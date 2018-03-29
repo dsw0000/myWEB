fec33e46493b55bcfef1e005079a9e9f9a0c7f88
Ivo Dai <ivo_d@icloud.com>
2018-03-29 16:12:22 +0800

    第一版页面

---
+++ backstage/datas/nav.js
@@ -0,0 +1,66 @@
+var navs = [{
+	"title": "基本元素",
+	"icon": "fa-cubes",
+	"spread": true,
+	"children": [{
+		"title": "按钮",
+		"icon": "&#xe641;",
+		"href": "html/button.html"
+	}, {
+		"title": "表单",
+		"icon": "&#xe63c;",
+		"href": "html/form.html"
+	}, {
+		"title": "表格",
+		"icon": "&#xe63c;",
+		"href": "html/table.html"
+	}, {
+		"title": "导航",
+		"icon": "&#xe609;",
+		"href": "html/nav.html"
+	}, {
+		"title": "辅助性元素",
+		"icon": "&#xe60c;",
+		"href": "html/auxiliar.html"
+	}]
+}, {
+	"title": "组件",
+	"icon": "fa-cogs",
+	"spread": false,
+	"children": [{
+		"title": "BTable",
+		"icon": "fa-table",
+		"href": "html/btable.html"
+	}, {
+		"title": "Navbar组件",
+		"icon": "fa-navicon",
+		"href": "html/navbar.html"
+	}, {
+		"title": "Tab组件",
+		"icon": "&#xe62a;",
+		"href": "html/tab.html"
+	}, {
+		"title": "Laytpl+Laypage",
+		"icon": "&#xe628;",
+		"href": "html/paging.html"
+	}]
+}, {
+	"title": "第三方组件",
+	"icon": "&#x1002;",
+	"spread": false,
+	"children": [{
+		"title": "iCheck组件",
+		"icon": "fa-check-square-o",
+		"href": "html/icheck.html"
+	}]
+},  {
+	"title": "其他",
+	"icon": "fa-stop-circle",
+	"href": "#",
+	"spread": false,
+	"children": [{
+		"title": "子窗体中打开选项卡",
+		"icon": "fa-github",
+		"href": "html/cop.html"
+	}]
+}];
